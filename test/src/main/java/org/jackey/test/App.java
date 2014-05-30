25f61cfd18f23210e7adf6ad36bde7aeb8cbd330
jackey90 <jackey90.hj@gmail.com>
2014-05-30 15:08:34 +0800

    create test repository

---
+++ test/src/main/java/org/jackey/test/App.java
@@ -0,0 +1,13 @@
+package org.jackey.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
